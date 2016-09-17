17eefa9d3e9bfb0ad40e2edc5736e827b568490a
LianweiMa <malianwei2009@163.com>
2016-09-17 22:12:56 +0800

    AE开发7-数据查询与选择

---
+++ QueryAndSelect/QueryAndSelect/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace QueryAndSelect
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axLicenseControl1 = new ESRI.ArcGIS.Controls.AxLicenseControl();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.axToolbarControl1 = new ESRI.ArcGIS.Controls.AxToolbarControl();
+            this.axMapControl1 = new ESRI.ArcGIS.Controls.AxMapControl();
+            ((System.ComponentModel.ISupportInitialize)(this.axLicenseControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axToolbarControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axMapControl1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axLicenseControl1
+            // 
+            this.axLicenseControl1.Enabled = true;
+            this.axLicenseControl1.Location = new System.Drawing.Point(536, 426);
+            this.axLicenseControl1.Name = "axLicenseControl1";
+            this.axLicenseControl1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axLicenseControl1.OcxState")));
+            this.axLicenseControl1.Size = new System.Drawing.Size(32, 32);
+            this.axLicenseControl1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(489, 94);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "点选";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(489, 158);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "圆选";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(489, 222);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "矩形选择";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(489, 286);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "多边形选择";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(489, 350);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "清除选择";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(489, 10);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "名称查询";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(314, 10);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(169, 21);
+            this.textBox1.TabIndex = 9;
+            // 
+            // axToolbarControl1
+            // 
+            this.axToolbarControl1.Location = new System.Drawing.Point(12, 5);
+            this.axToolbarControl1.Name = "axToolbarControl1";
+            this.axToolbarControl1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axToolbarControl1.OcxState")));
+            this.axToolbarControl1.Size = new System.Drawing.Size(265, 28);
+            this.axToolbarControl1.TabIndex = 8;
+            // 
+            // axMapControl1
+            // 
+            this.axMapControl1.Location = new System.Drawing.Point(12, 43);
+            this.axMapControl1.Name = "axMapControl1";
+            this.axMapControl1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axMapControl1.OcxState")));
+            this.axMapControl1.Size = new System.Drawing.Size(461, 416);
+            this.axMapControl1.TabIndex = 0;
+            this.axMapControl1.OnMouseDown += new ESRI.ArcGIS.Controls.IMapControlEvents2_Ax_OnMouseDownEventHandler(this.axMapControl1_OnMouseDown);
+            this.axMapControl1.OnAfterDraw += new ESRI.ArcGIS.Controls.IMapControlEvents2_Ax_OnAfterDrawEventHandler(this.axMapControl1_OnAfterDraw);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(580, 470);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.axToolbarControl1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.axLicenseControl1);
+            this.Controls.Add(this.axMapControl1);
+            this.Name = "Form1";
+            this.Text = "Query and Selection";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.axLicenseControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axToolbarControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axMapControl1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ESRI.ArcGIS.Controls.AxMapControl axMapControl1;
+        private ESRI.ArcGIS.Controls.AxLicenseControl axLicenseControl1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private ESRI.ArcGIS.Controls.AxToolbarControl axToolbarControl1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ QueryAndSelect/QueryAndSelect/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using ESRI.ArcGIS.Geometry;
+using ESRI.ArcGIS.Controls;
+using ESRI.ArcGIS.Geodatabase;
+using ESRI.ArcGIS.Carto;
+namespace QueryAndSelect
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        UInt16 m_Flag = 0;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            m_Flag = 1;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            m_Flag = 2;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            m_Flag = 3;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            m_Flag = 4;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+           
+            axMapControl1.Map.ClearSelection();
+            axMapControl1.Refresh();
+            m_Flag = 0;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            string strSearchName = this.textBox1.Text.Trim();
+            //MessageBox.Show(strSearchName);
+            IQueryFilter pQueryFilter = new QueryFilterClass();
+            pQueryFilter.WhereClause = "NAME ='" + strSearchName + "'";
+            IFeatureCursor pFeatureCussor;
+            ILayer pLayer=axMapControl1.Map.get_Layer(0);
+            IFeatureLayer pFeatureLayer=pLayer as IFeatureLayer;
+            IFeatureClass pFeatureClass=pFeatureLayer.FeatureClass;
+            pFeatureCussor = pFeatureClass.Search(pQueryFilter, true);
+            IFeature pFeature = pFeatureCussor.NextFeature();
+            if (pFeature != null)
+            {
+                axMapControl1.Map.SelectFeature(axMapControl1.get_Layer(0), pFeature);
+                axMapControl1.Refresh(esriViewDrawPhase.esriViewGeoSelection, null, null);
+            }
+        }
+
+        private void axMapControl1_OnMouseDown(object sender, ESRI.ArcGIS.Controls.IMapControlEvents2_OnMouseDownEvent e)
+        {
+            if (e.button == 1)
+            {
+                IGeometry pGeometry = null;
+                switch (m_Flag)
+                {
+                    case 1:
+                        IPoint pPt = new PointClass();
+                        pPt.X = e.mapX; pPt.Y = e.mapY;
+                        pGeometry = pPt as IGeometry;
+                        //axMapControl1.DrawShape(pGeometry);
+                        //axMapControl1.DrawText(pGeometry, "x=" + pPt.X.ToString("f") + "y=" + pPt.Y.ToString("f"));
+                        break;
+                    case 2:
+                        pGeometry = axMapControl1.TrackCircle();
+                        break;
+                    case 3:
+                        pGeometry = axMapControl1.TrackRectangle();
+                        break;
+                    case 4:
+                        pGeometry = axMapControl1.TrackPolygon();
+                        break;
+                }
+                if (pGeometry != null)
+                {
+                    //ISelectionEnvironment pSelectionEnvironment = new SelectionEnvironmentClass();
+                    //pSelectionEnvironment.PointSearchDistance = 100;
+                    axMapControl1.Map.SelectByShape(pGeometry, null, false);
+                    axMapControl1.Refresh();
+                    //axMapControl1.Refresh(esriViewDrawPhase.esriViewGeoSelection,null,null);
+                }
+            }
+            if (e.button == 4)
+            {
+                axMapControl1.MousePointer = esriControlsMousePointer.esriPointerHand;
+                axMapControl1.Pan();
+                axMapControl1.MousePointer = esriControlsMousePointer.esriPointerDefault;
+            }
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+/*
+            OpenFileDialog pOpenFileDialog=new OpenFileDialog();
+            pOpenFileDialog.Title = "Select file...";
+            pOpenFileDialog.Filter = "mxd file|*.mxd|All file|*.*";
+            if (pOpenFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string strMxdFilename = pOpenFileDialog.FileName;
+                axMapControl1.LoadMxFile(strMxdFilename);
+            }*/
+        }
+
+        private void axMapControl1_OnAfterDraw(object sender, IMapControlEvents2_OnAfterDrawEvent e)
+        {
+            axMapControl1.Focus();
+        }
+    }
+}
