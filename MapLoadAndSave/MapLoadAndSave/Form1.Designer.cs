90cfa9b58f41d4f9fdc81d4a1798e82fa9c8b3fc
LianweiMa <malianwei2009@163.com>
2016-09-17 22:12:11 +0800

    AE开发1-2-地图的加载和保存

---
+++ MapLoadAndSave/MapLoadAndSave/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace MapLoadAndSave
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axMapControl1 = new ESRI.ArcGIS.Controls.AxMapControl();
+            this.axLicenseControl1 = new ESRI.ArcGIS.Controls.AxLicenseControl();
+            this.MapLoad = new System.Windows.Forms.Button();
+            this.MapSave = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.axMapControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axLicenseControl1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axMapControl1
+            // 
+            this.axMapControl1.Location = new System.Drawing.Point(13, 13);
+            this.axMapControl1.Name = "axMapControl1";
+            this.axMapControl1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axMapControl1.OcxState")));
+            this.axMapControl1.Size = new System.Drawing.Size(495, 452);
+            this.axMapControl1.TabIndex = 0;
+            // 
+            // axLicenseControl1
+            // 
+            this.axLicenseControl1.Enabled = true;
+            this.axLicenseControl1.Location = new System.Drawing.Point(569, 400);
+            this.axLicenseControl1.Name = "axLicenseControl1";
+            this.axLicenseControl1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axLicenseControl1.OcxState")));
+            this.axLicenseControl1.Size = new System.Drawing.Size(32, 32);
+            this.axLicenseControl1.TabIndex = 1;
+            // 
+            // MapLoad
+            // 
+            this.MapLoad.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.MapLoad.Location = new System.Drawing.Point(533, 61);
+            this.MapLoad.Name = "MapLoad";
+            this.MapLoad.Size = new System.Drawing.Size(86, 26);
+            this.MapLoad.TabIndex = 2;
+            this.MapLoad.Text = "加载地图文档";
+            this.MapLoad.UseVisualStyleBackColor = true;
+            this.MapLoad.Click += new System.EventHandler(this.MapLoad_Click);
+            // 
+            // MapSave
+            // 
+            this.MapSave.Location = new System.Drawing.Point(533, 214);
+            this.MapSave.Name = "MapSave";
+            this.MapSave.Size = new System.Drawing.Size(86, 23);
+            this.MapSave.TabIndex = 3;
+            this.MapSave.Text = "保存地图文档";
+            this.MapSave.UseVisualStyleBackColor = true;
+            this.MapSave.Click += new System.EventHandler(this.MapSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(631, 477);
+            this.Controls.Add(this.MapSave);
+            this.Controls.Add(this.MapLoad);
+            this.Controls.Add(this.axLicenseControl1);
+            this.Controls.Add(this.axMapControl1);
+            this.Name = "Form1";
+            this.Text = "地图操作";
+            ((System.ComponentModel.ISupportInitialize)(this.axMapControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axLicenseControl1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ESRI.ArcGIS.Controls.AxMapControl axMapControl1;
+        private ESRI.ArcGIS.Controls.AxLicenseControl axLicenseControl1;
+        private System.Windows.Forms.Button MapLoad;
+        private System.Windows.Forms.Button MapSave;
+    }
+}
+
---
+++ MapLoadAndSave/MapLoadAndSave/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using ESRI.ArcGIS.Carto;
+namespace MapLoadAndSave
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string strFilename;
+        IMapDocument pMapDocument = new MapDocument();
+        private void MapLoad_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog pOpenFileDialog=new OpenFileDialog();
+            pOpenFileDialog.InitialDirectory = @"D:\D_DataProcessingProject\ArcGIS\A_Test";
+            pOpenFileDialog.Filter = "Mxd file|*.mxd|All file|*.*";
+            pOpenFileDialog.RestoreDirectory = true;
+            pOpenFileDialog.FilterIndex = 1;
+            if (pOpenFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                strFilename=pOpenFileDialog.FileName;
+               this.Text = System.IO.Path.GetFileName(strFilename);
+               pMapDocument.Open(strFilename);
+               int iMapCount = pMapDocument.MapCount;
+               axMapControl1.Map = pMapDocument.get_Map(0);
+                //axMapControl1.AddShapeFile(System.IO.Path.GetDirectoryName(strFilename), System.IO.Path.GetFileNameWithoutExtension(strFilename));
+            }
+  
+        }
+
+        private void MapSave_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog pSaveFileDialog = new SaveFileDialog();
+            pSaveFileDialog.InitialDirectory = System.IO.Path.GetDirectoryName(strFilename);
+            pSaveFileDialog.Filter = "Mxd file|*.mxd|All file|*.*";
+            pSaveFileDialog.FilterIndex = 1;
+            if (pSaveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                strFilename=pSaveFileDialog.FileName;
+                pMapDocument.SaveAs(strFilename);
+            }
+        }
+    }
+}
